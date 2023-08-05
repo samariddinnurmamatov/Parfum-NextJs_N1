78135bdcc68dfb690a306f43deb70bd0a6d73e6f
“Samariddin <samariddinnurmamatov111@gmail.com>
2023-08-05 12:35:02 +0500

    Parfum

--- app/(public)/category/[categoryId]/page.jsx
+++ app/(public)/category/[categoryId]/page.jsx
@@ -6,13 +6,11 @@ import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const CategoryPage = () => {
-  const [search, setSearch] = useState("");
   const [products, setProducts] = useState({ total: 0, products: [] });
   const [page, setPage] = useState(1);
   const { categoryId } = useParams();
-  const Limit = 8;
-
-  const getProducts = async ({ categoryId, search }) => {
+  const Limit = 10;
+  const getCateProducts = async ({ categoryId }) => {
     try {
       const { data } = await request(`product?category=${categoryId}`);
       setProducts(data);
@@ -22,37 +20,27 @@ const CategoryPage = () => {
   };
 
   useEffect(() => {
-    getProducts({ categoryId, search });
-  }, [search, categoryId]);
+    getCateProducts({ categoryId });
+  }, [categoryId]);
 
-  const searchProduct = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const changePage = (current) => {
-    setPage(current);
-  };
   return (
     <div className="container" style={{ padding: "20px 10px" }}>
-      <div className="flex justify-between py-6">
+      <div className="flex justify-between">
         <input
           type="text"
           placeholder="Search..."
-          onChange={searchProduct}
           className="outline-none border-b-[1.4px]"
         />
-       
       </div>
-      <div className="grid grid-cols-1 min-[500px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 containr gap-3 p-2">
+      <div className="all_card_category">
         {products?.products?.slice((page - 1) * Limit, Limit).map((pr) => (
           <div
             key={pr._id}
-            className=" bg-opacity-80 text-black rounded p-0.5 border-black"
-            style={{border: "1px solid gray"}}
+            style={{ border: "1px solid gray" }}
           >
             <Image
               src={pr?.images?.url}
-              alt="product img"
+              alt="img"
               height={200}
               width={300}
               style={{
@@ -62,13 +50,15 @@ const CategoryPage = () => {
                 borderRadius: "3px 3px 0px 0px",
               }}
             />
-            <div className="py-2 px-4">
-              <h3 className="text-center w-full font-semibold text-xl">
+            <div className="py-2 px-5">
+              <h1 style={{fontWeight: "700"}} className="text-center">
                 {pr?.title}
-              </h3>
+              </h1>
               <p className="text-center">Miqdor: {pr?.sold}</p>
               <p className="text-center">Narx: {pr?.price}</p>
-              <button className="text-center py-1 px-3 rounded bg-white bg-opacity-20 hover:bg-opacity-30 hover:shadow-black-400 hover:shadow">{`Qo'shish`}</button>
+              <button
+                style={{ border: "1px solid gray", width: "100%" }}
+              >{`Qo'shish`}</button>
             </div>
           </div>
         ))}
@@ -76,7 +66,6 @@ const CategoryPage = () => {
       {products.total !== 0 && (
         <div className="py-5 flex justify-center">
           <Pagination
-            onChange={changePage}
             defaultCurrent={page}
             pageSize={Limit}
             total={products.total}
